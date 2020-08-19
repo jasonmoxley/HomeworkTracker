a5dfc2a8050b77e62c174458863f2eae7caa460d
jasonmoxley <jmoxley@live.unc.edu>
2020-08-19 12:57:58 -0400

    ready to deploy

--- hwtracker/src/components/Dashboard.js
+++ hwtracker/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import TaskList from './TaskList';
 import { fetchTasks, deleteTask, completeTask } from '../actions';
+import { Link } from 'react-router-dom';
 import history from '../history';
 import moment from 'moment';
 
@@ -59,6 +60,22 @@ class Dashboard extends React.Component {
                     <div className="ui active inverted dimmer">
                         <div className="ui large text loader">Loading</div>
                     </div>
+                </div>
+            );
+        } else if (tasks.length === 0) {
+            return (
+                <div style={{
+                    textAlign: "center",
+                    marginTop: "100px",
+                    width: "50%",
+                    marginLeft: "auto",
+                    marginRight: "auto"
+                }}>
+                    <h1 className="ui inverted header">
+                        <div style={{ marginBottom: "10px"}}>Create a task to get started</div>
+                    </h1>
+                    <div className="ui hidden divider"></div>
+                    <Link to="/CreateTask" className="ui huge inverted download button">Create Task</Link>
                 </div>
             );
         } else {
